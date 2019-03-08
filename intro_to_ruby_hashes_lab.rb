dc538933a3e8503f3c7e3c89a135cbe5b6ab1695
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-08 19:19:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,9 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :names => {
 	      :reading_railroad => {},
-	      :pennsylvania => {},
-	      :b_and_o => {},
-	      :shortline => {}
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortlirne_railroad => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
